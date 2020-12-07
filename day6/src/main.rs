4df31cd8a6106e9b292af1ee05c91bfb3c225d07
David Reed <d.reed@salesforce.com>
2020-12-06 19:44:04 -0700

    More elegant solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,68 +2,48 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 
-fn from_file(file_name: &str) -> Vec<HashSet<char>> {
+fn part_1(file_name: &str) -> usize {
     let mut file = File::open(file_name).unwrap();
     let mut content = String::new();
 
-    let mut vec = Vec::new();
-
     file.read_to_string(&mut content).unwrap();
-    for group in content.trim().split("\n\n") {
-        let mut hs = HashSet::new();
-        for c in group.chars() {
-            if c != '\n' {
-                hs.insert(c);
-            }
-        }
-
-        vec.push(hs);
-    }
-    vec
+    content
+        .trim()
+        .split("\n\n")
+        .map(|group| group.chars().filter(|&c| c != '\n').collect::<HashSet<char>>())
+        .map(|g| g.len())
+        .sum()
 }
 
-fn from_file_part_2(file_name: &str) -> Vec<HashSet<char>> {
+fn part_2(file_name: &str) -> usize {
     let mut file = File::open(file_name).unwrap();
     let mut content = String::new();
 
-    let mut vec = Vec::new();
-
     file.read_to_string(&mut content).unwrap();
-    for group in content.trim().split("\n\n") {
-        let mut hs = HashSet::new();
-        let mut first = true;
-        for person in group.split_whitespace() {
-            let mut this_person = HashSet::new();
-            for c in person.chars() {
-                this_person.insert(c);
-            }
-            if !first {
-                hs = hs.intersection(&this_person).cloned().collect();
-            } else {
-                hs = this_person;
-                first = false;
-            }
-        }
-
-        vec.push(hs);
-    }
-    vec
+    content
+        .trim()
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split_whitespace()
+                .map(|person| person.chars().collect::<HashSet<char>>())
+                .fold(None, |acc: Option<HashSet<char>>, x: HashSet<char>| {
+                    if let Some(a) = acc {
+                        Some(a.intersection(&x).cloned().collect())
+                    } else {
+                        Some(x)
+                    }
+                })
+        })
+        .map(|s| s.unwrap().len())
+        .sum()
 }
-
 
 fn main() {
-    let groups = from_file("input.txt");
-
     println!(
         "Sum of group counts: {}",
-        groups.iter().map(|g| g.len()).sum::<usize>()
+        part_1("input.txt")
     );
 
-    let groups_part_2 = from_file_part_2("input.txt");
-
-    println!(
-        "Sum of group counts (part 2): {}",
-        groups_part_2.iter().map(|g| g.len()).sum::<usize>()
-    );
-
+    println!("Sum of group counts (part 2): {}", part_2("input.txt"));
 }
