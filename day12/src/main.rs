59deeb9d7e51d3aebdbb942773d993cd6880d2e9
David Reed <david@ktema.org>
2020-12-12 14:40:35 -0700

    Day 12, Part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,54 +3,94 @@ use std::io::Read;
 
 const HEADINGS: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
+#[derive(Clone, Debug)]
+struct Point(i32, i32);
+
 struct Ship {
     heading: i32,
-    x: i32,
-    y: i32,
+    pos: Point,
+    waypoint: Point,
+    mode: Mode,
+}
+
+enum Mode {
+    Normal,
+    Waypoint,
 }
 
 impl Ship {
-    pub fn new() -> Ship {
+    pub fn new(mode: Mode) -> Ship {
         Ship {
             heading: 90,
-            x: 0,
-            y: 0,
+            pos: Point(0, 0),
+            waypoint: Point(10, -1),
+            mode,
         }
     }
+
+    fn perform_move(&mut self, delta_x: i32, delta_y: i32) {
+        if let Mode::Normal = self.mode {
+            self.pos.0 += delta_x;
+            self.pos.1 += delta_y;
+        } else {
+            self.waypoint.0 += delta_x;
+            self.waypoint.1 += delta_y;
+        }
+    }
+
     pub fn execute(&mut self, cmd: &str) {
         let (order, operand) = cmd.split_at(1);
         let amount = operand.parse::<i32>().unwrap();
 
         match order {
-            "N" => {
-                self.y -= amount;
-            }
-            "S" => {
-                self.y += amount;
-            }
-            "W" => {
-                self.x -= amount;
-            }
-            "E" => {
-                self.x += amount;
-            }
+            "N" => self.perform_move(0, -amount),
+            "S" => self.perform_move(0, amount),
+            "W" => self.perform_move(-amount, 0),
+            "E" => self.perform_move(amount, 0),
             "L" => {
-                self.heading -= amount;
+                if let Mode::Normal = self.mode {
+                    self.heading -= amount;
 
-                if self.heading < 0 {
-                    self.heading = 360 + self.heading;
-                    self.heading = self.heading % 360;
+                    if self.heading < 0 {
+                        self.heading = 360 + self.heading;
+                        self.heading = self.heading % 360;
+                    }
+                } else {
+                    let rotation = amount % 360;
+                    match rotation {
+                        270 => self.waypoint = Point(-self.waypoint.1, self.waypoint.0),
+                        180 => self.waypoint = Point(-self.waypoint.0, -self.waypoint.1),
+                        90 => self.waypoint = Point(self.waypoint.1, -self.waypoint.0),
+                        _ => panic!("Invalid rotation {}", rotation),
+                    };
                 }
             }
             "R" => {
-                self.heading += amount;
-                if self.heading >= 360 {
-                    self.heading = self.heading % 360;
+                if let Mode::Normal = self.mode {
+                    self.heading += amount;
+                    if self.heading >= 360 {
+                        self.heading = self.heading % 360;
+                    }
+                } else {
+                    let rotation = amount % 360;
+                    match rotation {
+                        90 => self.waypoint = Point(-self.waypoint.1, self.waypoint.0),
+                        180 => self.waypoint = Point(-self.waypoint.0, -self.waypoint.1),
+                        270 => self.waypoint = Point(self.waypoint.1, -self.waypoint.0),
+                        _ => panic!("Invalid rotation {}", rotation),
+                    };
                 }
             }
             "F" => {
-                self.x += amount * HEADINGS[(self.heading / 90) as usize].0;
-                self.y += amount * HEADINGS[(self.heading / 90) as usize].1;
+                if let Mode::Normal = self.mode {
+                    self.perform_move(
+                        amount * HEADINGS[(self.heading / 90) as usize].0,
+                        amount * HEADINGS[(self.heading / 90) as usize].1,
+                    )
+                } else {
+                    self.pos.0 += amount * self.waypoint.0;
+                    self.pos.1 += amount * self.waypoint.1;
+                }
             }
             _ => panic!("Invalid instruction {}", order),
         }
@@ -61,17 +101,25 @@ fn main() {
     let mut file = File::open("input.txt").unwrap();
     let mut content = String::new();
 
-    let mut ship = Ship::new();
+    let mut ship = Ship::new(Mode::Normal);
+    let mut ship_part2 = Ship::new(Mode::Waypoint);
 
     file.read_to_string(&mut content).unwrap();
     for cmd in content.trim().lines() {
         ship.execute(cmd);
+        ship_part2.execute(cmd);
     }
 
     println!(
-        "Got Manhattan distance: {} for position x: {}, y: {}",
-        ship.x.abs() + ship.y.abs(),
-        ship.x,
-        ship.y
+        "Part 1: got Manhattan distance: {} for position x: {}, y: {}",
+        ship.pos.0.abs() + ship.pos.1.abs(),
+        ship.pos.0,
+        ship.pos.1
+    );
+    println!(
+        "Part 2: got Manhattan distance: {} for position x: {}, y: {}",
+        ship_part2.pos.0.abs() + ship_part2.pos.1.abs(),
+        ship_part2.pos.0,
+        ship_part2.pos.1
     );
 }
